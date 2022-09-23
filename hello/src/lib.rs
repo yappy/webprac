f2433e6a22bc0c8f82899d57880a098dfa4d92cd
yappy <superunyo@yahoo.co.jp>
2022-09-24 07:16:36 +0900

    Draw png image
    - Include base64 png image into binary.
    - Still buggy because we should wait for loading before drawing.

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,4 +1,5 @@
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
 
 #[wasm_bindgen]
 extern "C" {
@@ -8,4 +9,27 @@ extern "C" {
 #[wasm_bindgen]
 pub fn greet(name: &str) {
     alert(&format!("Hello, {}!", name));
+}
+
+#[wasm_bindgen(start)]
+pub fn start() {
+    let document = web_sys::window().unwrap().document().unwrap();
+    let canvas = document.get_element_by_id("canvas").unwrap();
+    let canvas: web_sys::HtmlCanvasElement = canvas
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .map_err(|_| ())
+        .unwrap();
+
+    const BASE64STR: &str = include_str!("unyounyo.txt");
+    let img = web_sys::HtmlImageElement::new().unwrap();
+    img.set_src(&format!("data:image/png;base64,{}", BASE64STR));
+
+    let context = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap();
+
+    context.draw_image_with_html_image_element(&img, 0.0, 0.0).unwrap();
 }
